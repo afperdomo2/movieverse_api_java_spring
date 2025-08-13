6c2eedaab424b3fecca5b8e71d29530ac406f22b
alvarado0925 <alvarado0925@gmail.com>
2025-08-13 18:32:12 -0500

    Agregar clase MovieEntity con atributos y métodos para la entidad de película

--- src/main/java/com/afperdomo2/movieverse/MovieverseApplication.java
+++ src/main/java/com/afperdomo2/movieverse/MovieverseApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieverseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieverseApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/afperdomo2/movieverse/persistence/entity/MovieEntity.java
@@ -0,0 +1,86 @@
+package com.afperdomo2.movieverse.persistence.entity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "movies")
+public class MovieEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false, precision = 3)
+    private Integer duration;
+
+    @Column(nullable = false, length = 50)
+    private String genre;
+
+    @Column(name = "release_date", nullable = false)
+    private LocalDate releaseDate;
+
+    @Column(precision = 3, scale = 2)
+    private BigDecimal classification;
+
+    @Column(nullable = false, length = 1)
+    private String status;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Integer duration) {
+        this.duration = duration;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+
+    public LocalDate getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(LocalDate releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public BigDecimal getClassification() {
+        return classification;
+    }
+
+    public void setClassification(BigDecimal classification) {
+        this.classification = classification;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
