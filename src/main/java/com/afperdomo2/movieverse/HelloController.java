93ba71de62a4ced56d94c805245282ff7fa278ef
alvarado0925 <alvarado0925@gmail.com>
2025-08-13 17:13:16 -0500

    Refactor HelloController to inject platform name and update MovieverseAiService to accept platform parameter for greeting generation. Add application properties for dev and prod environments.

--- src/main/java/com/afperdomo2/movieverse/HelloController.java
+++ src/main/java/com/afperdomo2/movieverse/HelloController.java
@@ -1,18 +1,22 @@
 package com.afperdomo2.movieverse;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    private final String platform;
     private final MovieverseAiService movieverseAiService;
 
-    public HelloController(MovieverseAiService movieverseAiService) {
+    public HelloController(@Value("${spring.application.name}") String platform,
+            MovieverseAiService movieverseAiService) {
+        this.platform = platform;
         this.movieverseAiService = movieverseAiService;
     }
 
     @GetMapping("/")
     public String sayHello() {
-        return movieverseAiService.generateGreeting();
+        return movieverseAiService.generateGreeting(this.platform);
     }
 }
--- src/main/java/com/afperdomo2/movieverse/MovieverseAiService.java
+++ src/main/java/com/afperdomo2/movieverse/MovieverseAiService.java
@@ -1,13 +1,14 @@
 package com.afperdomo2.movieverse;
 
 import dev.langchain4j.service.UserMessage;
+import dev.langchain4j.service.V;
 import dev.langchain4j.service.spring.AiService;
 
 @AiService
 public interface MovieverseAiService {
     @UserMessage("""
-            Genera un saludo de bienvenida a la plataforma de gestión de películas Movieverse.
+            Genera un saludo de bienvenida a la plataforma de gestión de películas {{platform}}.
             Usa menos de 120 caracteres y hazlo con un estilo amigable.
             """)
-    String generateGreeting();
+    String generateGreeting(@V("platform") String platform);
 }
