9652ab171f00095fd99cbb512d89f1426393b157
alvarado0925 <alvarado0925@gmail.com>
2025-08-13 18:57:21 -0500

    Agregar archivo de configuración de base de datos y datos iniciales para la entidad Movie

---
+++ src/main/java/com/afperdomo2/movieverse/persistence/crud/CrudMovieEntity.java
@@ -0,0 +1,9 @@
+package com.afperdomo2.movieverse.persistence.crud;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.afperdomo2.movieverse.persistence.entity.MovieEntity;
+
+public interface CrudMovieEntity extends CrudRepository<MovieEntity, Long> {
+
+}
